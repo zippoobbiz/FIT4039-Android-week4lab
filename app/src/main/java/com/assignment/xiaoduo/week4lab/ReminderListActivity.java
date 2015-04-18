a266c42db26610758a4d10aafd1fe375eff13d20
Xiaoduo <zippoobbiz@gmail.com>
2015-04-18 22:56:19 +1000

    version2
    fulfill requirements in week4

--- app/src/main/java/com/assignment/xiaoduo/week4lab/ReminderListActivity.java
+++ app/src/main/java/com/assignment/xiaoduo/week4lab/ReminderListActivity.java
@@ -7,6 +7,8 @@ import android.provider.Telephony;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -16,9 +18,12 @@ import java.util.List;
 public class ReminderListActivity extends Activity {
 
     public final int ADD_REMINDER_CODE = 1;
+    public final int EDIT_REMINDER_CODE = 2;
     List<Reminder> ReminderList;
     ListView reminderList_lv;
     ReminderAdapter adapter;
+    private int currentPosition = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +33,17 @@ public class ReminderListActivity extends Activity {
         ReminderList = new ArrayList<Reminder>();
         adapter = new ReminderAdapter(this, ReminderList);
         reminderList_lv.setAdapter(adapter);
+        reminderList_lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+                                    long arg3) {
+                currentPosition = arg2;
+                Intent intent = new Intent(ReminderListActivity.this, ShowReminderActivity.class);
+                intent.putExtra("reminder",ReminderList.get(arg2));
+                startActivityForResult(intent, EDIT_REMINDER_CODE);
+            }
+
+        });
 
 
     }
@@ -65,6 +81,17 @@ public class ReminderListActivity extends Activity {
             if(resultCode == RESULT_OK){
                 Reminder r = (Reminder)data.getSerializableExtra("reminder");
                 ReminderList.add(r);
+                adapter.notifyDataSetChanged();
+                Log.i("ReminderListActivity", "onActivityResult ADD_REMINDER_CODE RESULT_OK ReminderList.size = " + ReminderList.size());
+            }
+            if (resultCode == RESULT_CANCELED) {
+                //Write your code if there's no result
+            }
+        }else if(requestCode == EDIT_REMINDER_CODE)
+        {
+            if(resultCode == RESULT_OK){
+                Reminder r = (Reminder)data.getSerializableExtra("reminder");
+                ReminderList.set(currentPosition,r);
                 adapter.notifyDataSetChanged();
                 Log.i("ReminderListActivity", "onActivityResult ADD_REMINDER_CODE RESULT_OK ReminderList.size = " + ReminderList.size());
             }
